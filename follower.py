5a2f8eaab04309d2e6f870bfc8b2c0d312c45423
Upgwades <william.j.irwin10@gmail.com>
2018-02-19 14:32:27 -0500

    updated, zed and follower working on the car

--- follower.py
+++ follower.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Author: Will Irwin
 Last Modified: 2/12/2018
@@ -9,35 +10,44 @@ Outputs:
 """
 import cv2
 import rospy as rp
-from rospy.numpy_msg import numpy_msg
 import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+
 
 # def preprocess():
 #
 # def find_line():
 #
 # def follow_line():
-#
-#
-# def callback(data):
-    # rp.loginfo(rp.get_caller_id())+" I heard %s", data.data)
+
+
+def callback(data):
+    bridge = CvBridge()
+    try:
+	    #cv2.namedWindow("Image window", cv2.WINDOW_NORMAL)
+        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+        cv2.imshow("title",cv_image)
+        cv2.waitKey(0)
+    except CvBridgeError as e:
+        print(e)
 
     # img = preprocess(data.data)
     # line = find_line(img)
     # dir = follow_line(line)
-    cv2.imshow("ZED",data.data)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
     # while not rp.is_shutdown():
-    output = dir
-    rp.loginfo(output)
-    pub.publish(output)
+    # output = dir
+
+    # rp.loginfo(output)
+    # pub.publish(output)
 
 
 def listener():
-    rp.init_node('Follower'Anonymous = True)
-    rp.Subscriber('zed_receiver',numpy_msg(Ints),callback)
-    # pub = rp.Publisher('movement_instructions',numpy_msg(Floats),queue_size = 10)
+    rp.init_node('Follower',anonymous = True)
+    rp.Subscriber('zed_images',Image,callback)
+    pub = rp.Publisher('movement_instructions',Image)
     rp.spin()
 
 
@@ -46,3 +56,4 @@ if __name__=="__main__":
         listener()
     except rp.ROSInterruptException:
         pass
+    cv2.destroyAllWindows()
--- zed.py
+++ zed.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Author: Will Irwin
 Last Modified: 2/12/2018
@@ -9,27 +10,32 @@ Outputs:
 """
 import cv2
 import rospy as rp
-from rospy.numpy_msg import numpy_msg
 import numpy as np
+from sensor_msgs.msg import Image
+# import sensor_msgs.msg.Image
+from cv_bridge import CvBridge, CvBridgeError
+bridge = CvBridge()
 
-
+cap = cv2.VideoCapture(0)
 def get_zed_data():
     rp.init_node('Zed',anonymous = True)
-    pub = rp.Publisher('zed_images',numpy_msg(Ints),queue_size = 10)
-    rate = rp.rate(21)#21Hz
+    pub = rp.Publisher('zed_images',Image,queue_size=10)
+    rate = rp.Rate(21)#21Hz
 
-    while not rp.is_shutdown():
-        cap = cv2.VideoCapture(0)
-        while(cap.isOpened()):
-            ret, frame = cap.read()
-            output = frame
-            rp.loginfo(output)
-            pub.publish(output)
-            rate.sleep()
-        cap.release()
+    while (not rp.is_shutdown()) and (cap.isOpened()):
+        ret, frame = cap.read()
+        try:
+            output = bridge.cv2_to_imgmsg(frame, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        #rp.loginfo(output)
+        pub.publish(output)
+        rate.sleep()
+    cap.release()
 
 if __name__=="__main__":
     try:
         get_zed_data()
     except rp.ROSInterruptException:
-        pass
+        cap.release()
+
