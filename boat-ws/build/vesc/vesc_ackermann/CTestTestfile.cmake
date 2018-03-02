d71c711a8b44ae1f52c912f83eca7454203b6ac0
Brett Glendenning <brettg0396@gmail.com>
2018-03-02 13:16:03 -0500

    Literally Everything (Again)

--- boat-ws/build/vesc/vesc_ackermann/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/racecar/AV_Invisible_Boat/boat-ws/src/vesc/vesc_ackermann
-# Build directory: /home/racecar/AV_Invisible_Boat/boat-ws/build/vesc/vesc_ackermann
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
