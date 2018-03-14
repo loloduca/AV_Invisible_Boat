00857e545dcb3ca4046affb746785198e63ae436
Brett Glendenning <brettg0396@gmail.com>
2018-03-14 14:13:56 -0400

    what is happening

--- boat-ws/build/racecar/ackermann_cmd_mux/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/racecar/AV_Invisible_Boat/boat-ws/src/racecar/ackermann_cmd_mux
-# Build directory: /home/racecar/AV_Invisible_Boat/boat-ws/build/racecar/ackermann_cmd_mux
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- boat-ws/build/racecar/racecar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/racecar/AV_Invisible_Boat/boat-ws/src/racecar/racecar
-# Build directory: /home/racecar/AV_Invisible_Boat/boat-ws/build/racecar/racecar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
